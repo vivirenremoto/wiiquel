46f0444579da0250850063973f0151bd19d34fb3
miquel camps orteza <gafeman@gmail.com>
2020-10-30 19:07:39 +0100

    FIX

--- main.js
+++ main.js
@@ -196,8 +196,7 @@ function openOption(current_option) {
 
 
     if (current_option == 'option1') {
-        html = '<h2>Latest Photo @vivirenremoto</h2><img src="https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/e35/s1080x1080/117092380_1349625508762097_15107043003097979_n.jpg?_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=105&_nc_ohc=b9JzS7u9ZQgAX8gfX70&oh=f5eb9d57162c75c4a2062778caa8e68d&oe=5F5E88DF" style="max-width:300px;">';// '<iframe width="560" height="315" src="https://www.youtube.com/embed/NlnFc54t-78?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-
+        html = '<h2>Latest Photo @vivirenremoto</h2><img src="https://vivirenremoto.github.io/wiiquel/photo.jpg" style="max-width:300px;">';
     } else if (current_option == 'option2') {
         html = '<h2>Latest Tweet @vivirenremoto</h2><iframe id="twitter-widget-0" scrolling="no" allowtransparency="true" allowfullscreen="true" class="" style="position: static; visibility: visible; width: 550px; height: 569px; display: block; flex-grow: 1;background:#fafafa;" title="Twitter Tweet" src="https://platform.twitter.com/embed/index.html?dnt=false&amp;embedId=twitter-widget-0&amp;frame=false&amp;hideCard=false&amp;hideThread=false&amp;id=1288317275683135488&amp;lang=en&amp;origin=https%3A%2F%2Fpublish.twitter.com%2F%3Fquery%3Dhttps%253A%252F%252Ftwitter.com%252Fvivirenremoto%252Fstatus%252F1288317275683135488%26widget%3DTweet&amp;theme=light&amp;widgetsVersion=223fc1c4%3A1596143124634&amp;width=550px" data-tweet-id="1288317275683135488" frameborder="0"></iframe>';
     } else if (current_option == 'option3') {
